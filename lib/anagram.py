0b68c06f87bdb212cc489f67c920c03b935e0439
Christopher Cantu <chrischimi02@gmail.com>
2024-05-03 20:36:35 -0500

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.word_sorted = sorted(self.word)
+
+    def match(self, words):
+        return [word for word in words if sorted(word.lower()) == self.word_sorted and word.lower() != self.word]
